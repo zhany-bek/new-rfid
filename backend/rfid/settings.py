e7d86073984bea7481b0acc7ef3fa11eae6cf933
Zhanybek Bekbolat <daveelfie@gmail.com>
2023-11-24 02:36:32 +0600

    Add Vercel specs

--- backend/rfid/settings.py
+++ backend/rfid/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 #LOGIN_REDIRECT_URL = '/dash'
 #LOGIN_URL = '/login'
